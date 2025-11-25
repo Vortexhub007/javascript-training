7eee82e324d5b58738cbe620a555c9299b4127b7
Vortex69400 <valentinmartin69400@gmail.com>
2025-11-25 13:27:24 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+// 
+console.log("Hello Word");
+console.log("Hello Word");
+
+const nom="toto";
+console.log(nom);
+
+console.log("Salut " + nom);
+const names=["Valentin", "Sarah", "Melissa"]
+console.log(names[0]);
+
+let age =18;
+
