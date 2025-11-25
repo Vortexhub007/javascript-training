432e103ef4c17e32952171740dd2aab1f1acd3d6
Vortex69400 <valentinmartin69400@gmail.com>
2025-11-25 14:01:07 +0100

    second commit

--- test.js
+++ test.js
@@ -9,5 +9,19 @@ console.log("Salut " + nom);
 const names=["Valentin", "Sarah", "Melissa"]
 console.log(names[0]);
 
-let age =18;
-
+let age = 18;
+if (typeof age !== "nombre") {
+    console.log("Entrer un age");
+}
+else if(age < 0) {
+    console.log("C'est un age plus petit que 0");
+}
+else if(age > 18) {
+    console.log("Tu as le droit de conduire");
+}
+else if(age === 18) {
+    console.log("Bon Anniversaire");
+}
+else {
+    console.log("Rentre à pied !");
+}
